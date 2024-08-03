93ccf60f87fa3ce1c332f11a5ab569dd7c5a3feb
Shubham Bhardwaj <shubham998845@gmail.com>
2024-08-03 13:27:30 +0530

    initial server

--- Space-App/server/src/server.js
+++ Space-App/server/src/server.js
@@ -1,2 +1,10 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
 const PORT = process.env.PORT || 8000;
-console.log(PORT);
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
