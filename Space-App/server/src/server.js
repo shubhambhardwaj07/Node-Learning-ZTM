687b2a9f350784b82016538906349c541938c403
Shubham Bhardwaj <shubham998845@gmail.com>
2024-08-06 22:51:51 +0530

    setting up the first planets model and cleanuop

--- Space-App/server/src/server.js
+++ Space-App/server/src/server.js
@@ -1,11 +1,20 @@
 const http = require("http");
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 // This is a better approach when we will use web sockets
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+async function startServer() {
+  // This is basically to always make sure we load the initially the data for first call
+  // when we have streams of data
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+  });
+}
+
+startServer();
